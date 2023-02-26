84f1288be3bcb619dfe96cd1dfbd97e3784a8acd
John Sanpe <sanpeqf@gmail.com>
2023-02-26 22:04:55 +0800

    Function: Added list benchmark and selftest.
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

---
+++ examples/list-benchmark.c
@@ -0,0 +1,98 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright(c) 2021 Sanpe <sanpeqf@gmail.com>
+ */
+
+#include "list.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/times.h>
+
+#define LIST_DEBUG 0
+#define TEST_LEN 1000000
+static LIST_HEAD(demo_list);
+
+struct list_demo {
+    struct list_head list;
+    unsigned int num;
+    unsigned long data;
+};
+
+#define list_to_demo(node) \
+    list_entry(node, struct list_demo, list)
+
+#if LIST_DEBUG
+static void node_dump(struct list_demo *node)
+{
+    printf("  %08d: data 0x%016lx\n", node->num, node->data);
+}
+#else
+# define node_dump(node) ((void)(node))
+#endif
+
+static void time_dump(int ticks, clock_t start, clock_t stop, struct tms *start_tms, struct tms *stop_tms)
+{
+    printf("  real time: %lf\n", (stop - start) / (double)ticks);
+    printf("  user time: %lf\n", (stop_tms->tms_utime - start_tms->tms_utime) / (double)ticks);
+    printf("  kern time: %lf\n", (stop_tms->tms_stime - start_tms->tms_stime) / (double)ticks);
+}
+
+static long demo_cmp(struct list_head *a, struct list_head *b, void *pdata)
+{
+    struct list_demo *demo_a = list_to_demo(a);
+    struct list_demo *demo_b = list_to_demo(b);
+    return demo_a->data - demo_b->data;
+}
+
+int main(void)
+{
+    struct list_demo *node, *tmp;
+    struct tms start_tms, stop_tms;
+    clock_t start, stop;
+    unsigned int count, ticks;
+    int ret = 0;
+
+    ticks = sysconf(_SC_CLK_TCK);
+
+    printf("Generate %d Node:\n", TEST_LEN);
+    start = times(&start_tms);
+    for (count = 0; count < TEST_LEN; ++count) {
+        node = malloc(sizeof(*node));
+        if ((ret = !node)) {
+            printf("insufficient memory\n");
+            goto error;
+        }
+
+        node->num = count;
+        node->data = ((unsigned long)rand() << 32) | rand();
+        node_dump(node);
+
+        list_add(&demo_list, &node->list);
+    }
+    stop = times(&stop_tms);
+    time_dump(ticks, start, stop, &start_tms, &stop_tms);
+
+    start = times(&start_tms);
+    printf("List sort:\n");
+    list_qsort(&demo_list, demo_cmp, NULL);
+    stop = times(&stop_tms);
+    time_dump(ticks, start, stop, &start_tms, &stop_tms);
+
+    start = times(&start_tms);
+    printf("List for each:\n");
+    list_for_each_entry(node, &demo_list, list)
+        node_dump(node);
+    stop = times(&stop_tms);
+    time_dump(ticks, start, stop, &start_tms, &stop_tms);
+
+    printf("Deletion All Node...\n");
+error:
+    list_for_each_entry_safe(node, tmp, &demo_list, list)
+        free(node);
+
+    if (!ret)
+        printf("Done.\n");
+
+    return ret;
+}
---
+++ examples/list-selftest.c
@@ -0,0 +1,218 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright(c) 2021 Sanpe <sanpeqf@gmail.com>
+ */
+
+#include "list.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * ARRAY_SIZE - get the number of elements in array.
+ * @arr: array to be sized.
+ */
+#define ARRAY_SIZE(arr) ( \
+    sizeof(arr) / sizeof((arr)[0]) \
+)
+
+#define TEST_LOOP 10
+
+struct list_test_node {
+    struct list_head list;
+    unsigned long num;
+};
+
+struct list_test_pdata {
+    struct list_test_node nodes[TEST_LOOP];
+};
+
+#define list_to_test(ptr) \
+    list_entry(ptr, struct list_test_node, list)
+
+static long list_test_sort(struct list_head *nodea, struct list_head *nodeb, void *pdata)
+{
+    struct list_test_node *testa = list_to_test(nodea);
+    struct list_test_node *testb = list_to_test(nodeb);
+    return testa->num - testb->num;
+}
+
+static int list_selftest(struct list_test_pdata *ldata)
+{
+    struct list_test_node *node, *nnode, *tnode;
+    struct list_head *list, *nlist, *tlist;
+    unsigned int count;
+
+    LIST_HEAD(test_head);
+
+    for (count = 0; count < ARRAY_SIZE(ldata->nodes); ++count) {
+        if (count % 1)
+            list_add_prev(&test_head, &ldata->nodes[count].list);
+        else
+            list_add(&test_head, &ldata->nodes[count].list);
+    }
+
+    list_qsort(&test_head, list_test_sort, NULL);
+
+    list_for_each(list, &test_head) {
+        node = list_to_test(list);
+        printf("list 'list_for_each' test: %lu\n", node->num);
+        if (node->num == TEST_LOOP / 2)
+            break;
+    }
+
+    tlist = list;
+    list_for_each_continue(list, &test_head) {
+        node = list_to_test(list);
+        printf("list 'list_for_each_continue' test: %lu\n", node->num);
+    }
+
+    list = tlist;
+    list_for_each_from(list, &test_head) {
+        node = list_to_test(list);
+        printf("list 'list_for_each_from' test: %lu\n", node->num);
+    }
+
+    list_for_each_reverse(list, &test_head) {
+        node = list_to_test(list);
+        printf("list 'list_for_each_reverse' test: %lu\n", node->num);
+        if (node->num == TEST_LOOP / 2)
+            break;
+    }
+
+    tlist = list;
+    list_for_each_reverse_continue(list, &test_head) {
+        node = list_to_test(list);
+        printf("list 'list_for_each_reverse_continue' test: %lu\n", node->num);
+    }
+
+    list = tlist;
+    list_for_each_reverse_from(list, &test_head) {
+        node = list_to_test(list);
+        printf("list 'list_for_each_reverse_from' test: %lu\n", node->num);
+    }
+
+    list_for_each_safe(list, nlist, &test_head) {
+        node = list_to_test(list);
+        printf("list 'list_for_each_safe' test: %lu\n", node->num);
+        if (node->num == TEST_LOOP / 2)
+            break;
+    }
+
+    tlist = list;
+    list_for_each_continue_safe(list, nlist, &test_head) {
+        node = list_to_test(list);
+        printf("list 'list_for_each_continue_safe' test: %lu\n", node->num);
+    }
+
+    list = tlist;
+    list_for_each_from_safe(list, nlist, &test_head) {
+        node = list_to_test(list);
+        printf("list 'list_for_each_from_safe' test: %lu\n", node->num);
+    }
+
+    list_for_each_reverse_safe(list, nlist, &test_head) {
+        node = list_to_test(list);
+        printf("list 'list_for_each_reverse_safe' test: %lu\n", node->num);
+        if (node->num == TEST_LOOP / 2)
+            break;
+    }
+
+    tlist = list;
+    list_for_each_reverse_continue_safe(list, nlist, &test_head) {
+        node = list_to_test(list);
+        printf("list 'list_for_each_reverse_continue_safe' test: %lu\n", node->num);
+    }
+
+    list = tlist;
+    list_for_each_reverse_from_safe(list, nlist, &test_head) {
+        node = list_to_test(list);
+        printf("list 'list_for_each_reverse_from_safe' test: %lu\n", node->num);
+    }
+
+    list_for_each_entry(node, &test_head, list) {
+        printf("list 'list_for_each_entry' test: %lu\n", node->num);
+        if (node->num == TEST_LOOP / 2)
+            break;
+    }
+
+    tnode = node;
+    list_for_each_entry_continue(node, &test_head, list) {
+        printf("list 'list_for_each_entry_continue' test: %lu\n", node->num);
+    }
+
+    node = tnode;
+    list_for_each_entry_from(node, &test_head, list) {
+        printf("list 'list_for_each_entry_from' test: %lu\n", node->num);
+    }
+
+    list_for_each_entry_reverse(node, &test_head, list) {
+        printf("list 'list_for_each_entry_reverse' test: %lu\n", node->num);
+        if (node->num == TEST_LOOP / 2)
+            break;
+    }
+
+    tnode = node;
+    list_for_each_entry_reverse_continue(node, &test_head, list) {
+        printf("list 'list_for_each_entry_reverse_continue' test: %lu\n", node->num);
+    }
+
+    node = tnode;
+    list_for_each_entry_reverse_from(node, &test_head, list) {
+        printf("list 'list_for_each_entry_reverse_from' test: %lu\n", node->num);
+    }
+
+    list_for_each_entry_safe(node, nnode, &test_head, list) {
+        printf("list 'list_for_each_entry_safe' test: %lu\n", node->num);
+        if (node->num == TEST_LOOP / 2)
+            break;
+    }
+
+    tnode = node;
+    list_for_each_entry_continue_safe(node, nnode, &test_head, list) {
+        printf("list 'list_for_each_entry_continue_safe' test: %lu\n", node->num);
+    }
+
+    node = tnode;
+    list_for_each_entry_from_safe(node, nnode, &test_head, list) {
+        printf("list 'list_for_each_entry_form_safe' test: %lu\n", node->num);
+    }
+
+    list_for_each_entry_reverse_safe(node, nnode, &test_head, list) {
+        printf("list 'list_for_each_entry_reverse_safe' test: %lu\n", node->num);
+        if (node->num == TEST_LOOP / 2)
+            break;
+        list_del(&node->list);
+    }
+
+    tnode = node;
+    list_for_each_entry_reverse_continue_safe(node, nnode, &test_head, list) {
+        printf("list 'list_for_each_entry_reverse_continue_safe' test: %lu\n", node->num);
+    }
+
+    node = tnode;
+    list_for_each_entry_reverse_from_safe(node, nnode, &test_head, list) {
+        printf("list 'list_for_each_entry_reverse_form_safe' test: %lu\n", node->num);
+        list_del(&node->list);
+    }
+
+    return 0;
+}
+
+int main(void)
+{
+    struct list_test_pdata *ldata;
+    unsigned int count;
+    int retval;
+
+    ldata = malloc(sizeof(struct list_test_pdata));
+    if (!ldata)
+        return -1;
+
+    for (count = 0; count < ARRAY_SIZE(ldata->nodes); ++count)
+        ldata->nodes[count].num = count;
+
+    retval = list_selftest(ldata);
+    free(ldata);
+
+    return retval;
+}
