a96b1af3a1e18d61d81b7d6d0a3d0f565d37b1a5
John Sanpe <sanpeqf@gmail.com>
2023-03-01 23:48:45 +0800

    Function: Added align macro header.
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

---
+++ include/bfdev/align.h
@@ -0,0 +1,42 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright(c) 2023 John Sanpe <sanpeqf@gmail.com>
+ */
+
+#ifndef _BFDEV_ALIGN_H_
+#define _BFDEV_ALIGN_H_
+
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * align_low/high - aligned value.
+ * @size: value to aligned.
+ * @align: alignment size.
+ */
+#define align_low(size, align) ({ \
+    (size) & ~((align) - 1); \
+})
+
+#define align_high(size, align) ({ \
+    typeof(align) _align = (align); \
+    ((size) + (_align - 1)) & ~(_align - 1); \
+})
+
+#define align_ptr_low(ptr, align) ({ \
+    (typeof(ptr))align_low((size_t)(ptr), align); \
+})
+
+#define align_ptr_high(ptr, align) ({ \
+    (typeof(ptr))align_high((size_t)(ptr), align); \
+})
+
+#define align_check(size, align) ( \
+    !((size) & ((align) - 1)) \
+)
+
+#define align_ptr_check(ptr, align) ( \
+    align_check((size_t)(ptr), align) \
+)
+
+#endif /* _BFDEV_ALIGN_H_ */
