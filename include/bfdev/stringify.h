c591107496317d43110dfc85aa18cf79f8573405
John Sanpe <sanpeqf@gmail.com>
2023-03-05 20:53:28 +0800

    feat: added indirect stringification support
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

---
+++ include/bfdev/stringify.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright(c) 2023 John Sanpe <sanpeqf@gmail.com>
+ */
+
+#ifndef _BFDEV_STRINGIFY_H_
+#define _BFDEV_STRINGIFY_H_
+
+/*
+ * Indirect stringification.  Doing two levels allows the parameter to be a
+ * macro itself.  For example, compile with -DFOO=bar, __stringify(FOO)
+ * converts to "bar".
+ */
+
+#define ___stringify(x...) #x
+#define __stringify(x...) ___stringify(x)
+
+#endif /* _BFDEV_STRINGIFY_H_ */
