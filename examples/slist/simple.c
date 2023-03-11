fcca2c90c2d43d621ef5c1642e27dcdd8d7a0966
John Sanpe <sanpeqf@gmail.com>
2023-03-11 20:32:36 +0800

    feat: added single list support
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

---
+++ examples/slist/simple.c
@@ -0,0 +1,51 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright(c) 2023 John Sanpe <sanpeqf@gmail.com>
+ */
+
+#include <bfdev/bfdev.h>
+#include <bfdev/slist.h>
+
+#define TEST_LEN 100
+static SLIST_HEAD(demo_list);
+
+struct list_sample {
+    struct slist_head list;
+    unsigned int num;
+    unsigned long data;
+};
+
+#define list_to_sample(node) \
+    container_of(node, struct list_sample, slist)
+
+int main(void)
+{
+    struct list_sample *node, *tmp;
+    unsigned int count;
+    int ret = 0;
+
+    printf("Generate %d Node:\n", TEST_LEN);
+    for (count = 0; count < TEST_LEN; ++count) {
+        node = malloc(sizeof(*node));
+        if ((ret = !node)) {
+            printf("insufficient memory\n");
+            goto error;
+        }
+
+        node->num = count;
+        node->data = ((unsigned long)rand() << 32) | rand();
+        slist_add(&demo_list, &node->list);
+    }
+
+    slist_for_each_entry(node, &demo_list, list)
+        printf("  %04u: 0x%016lx\n", node->num, node->data);
+
+    printf("Deletion All Node...\n");
+error:
+    slist_for_each_entry_safe(node, tmp, &demo_list, list) {
+        slist_del(&demo_list, &node->list);
+        free(node);
+    }
+
+    return ret;
+}
---
+++ src/slist-debug.c
@@ -0,0 +1,29 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright(c) 2023 John Sanpe <sanpeqf@gmail.com>
+ */
+
+#include <bfdev/bfdev.h>
+#include <bfdev/slist.h>
+
+bool slist_debug_add_check(struct slist_head *node, struct slist_head *new)
+{
+    if (unlikely(new->next && new->next == node->next)) {
+        fprintf(stderr, "slist_add corruption (%p) new->next should not be next (%p)\n",
+            new, node);
+        return false;
+    }
+
+    return true;
+}
+
+bool slist_debug_del_check(struct slist_head *node)
+{
+    if (unlikely(node->next == POISON_SLIST)) {
+        fprintf(stderr, "slist_del corruption (%p) node->next should not be POISON_SLIST (%p)\n",
+            node, POISON_SLIST);
+        return false;
+    }
+
+    return true;
+}
