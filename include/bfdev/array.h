49a8a7dfcee367ef40c492a835a87855d2bfda2e
ffashion <helloworldffashion@gmail.com>
2023-03-25 00:53:48 +0800

    feat array: support array

---
+++ include/bfdev/array.h
@@ -0,0 +1,38 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+* Copyright(c) 2023 ffashion <helloworldffashion@gmail.com>
+*/
+
+#ifndef _BFDEV_ARRAY_H_
+#define _BFDEV_ARRAY_H_
+
+#include "bfdev.h"
+#include "stddef.h"
+#include "errno.h"
+#include "string.h"
+#include "allocator.h"
+#include "galloctor.h"
+
+
+typedef struct bfdev_array_s bfdev_array_t;
+
+struct bfdev_array_s{
+    void *elts;
+    unsigned nelts;
+    size_t size;
+    unsigned nalloc;
+    allocator_t *allocator;
+};
+
+bfdev_array_t *bfdev_array_create(allocator_t *allocator, unsigned n, size_t size);
+void bfdev_array_destroy(bfdev_array_t *a);
+void *bfdev_array_push(bfdev_array_t *a);
+void *bfdev_array_push_n(bfdev_array_t *a, unsigned n);
+
+static inline void
+bfdev_array_reset(bfdev_array_t *a)
+{
+    a->nelts = 0;
+}
+
+#endif /*_BFDEV_ARRAY_H_*/
