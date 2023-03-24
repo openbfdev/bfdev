6e6ca656a747e953a620bcdf1cc819bb126fda74
John Sanpe <sanpeqf@gmail.com>
2023-03-25 01:47:32 +0800

    feat bytes: added byte size into stdint
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- include/bfdev/stdint.h
+++ include/bfdev/stdint.h
@@ -12,4 +12,17 @@
 # include <stdint.h>
 #endif
 
+#define BFDEV_BYTES_PER_CHAR        sizeof(char)
+#define BFDEV_BYTES_PER_SHORT       sizeof(short)
+#define BFDEV_BYTES_PER_INT         sizeof(int)
+#define BFDEV_BYTES_PER_LONG        sizeof(long)
+#define BFDEV_BYTES_PER_LONG_LONG   sizeof(long long)
+
+#define BFDEV_BYTES_PER_U8          sizeof(uint8_t)
+#define BFDEV_BYTES_PER_U16         sizeof(uint16_t)
+#define BFDEV_BYTES_PER_U32         sizeof(uint32_t)
+#define BFDEV_BYTES_PER_U64         sizeof(uint64_t)
+#define BFDEV_BYTES_PER_UINTMAX     sizeof(uintmax_t)
+#define BFDEV_BYTES_PER_UINTPTR     sizeof(uintptr_t)
+
 #endif /* _BFDEV_STDINT_H_ */
