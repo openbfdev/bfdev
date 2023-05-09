ccf148b646da26cf81b6cc77779c8655965d56fb
John Sanpe <sanpeqf@gmail.com>
2023-05-09 20:58:50 +0800

    feat bitops: added builtin functions support
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

---
+++ include/bfdev/asm-generic/builtin-fls.h
@@ -0,0 +1,26 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#ifndef _BFDEV_ASM_GENERIC_BUILTIN_FLS_H_
+#define _BFDEV_ASM_GENERIC_BUILTIN_FLS_H_
+
+#include <bfdev/bits.h>
+#include <bfdev/attributes.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if !__has_builtin(__builtin_clzl)
+# error "compiler not supported clzl"
+#endif
+
+#define bfdev_arch_fls bfdev_arch_fls
+static __always_inline unsigned int bfdev_arch_fls(unsigned long value)
+{
+	return value ? BFDEV_BITS_PER_LONG - __builtin_clzl(value) : 0;
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _BFDEV_ASM_GENERIC_BUILTIN_FLS_H_ */
