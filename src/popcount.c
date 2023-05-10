a0663d885197a5d4f0f30f9d7b4f36e56171c57a
John Sanpe <sanpeqf@gmail.com>
2023-05-10 20:25:32 +0800

    rename popcount: batch added bfdev prefix
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- src/popcount.c
+++ src/popcount.c
@@ -6,7 +6,7 @@
 #include <bfdev/popcount.h>
 #include <export.h>
 
-export const uint8_t popcount_table[256] = {
+export const uint8_t bfdev_popcount_table[256] = {
     [0x00] = 0, [0x01] = 1, [0x02] = 1, [0x03] = 2,
     [0x04] = 1, [0x05] = 2, [0x06] = 2, [0x07] = 3,
     [0x08] = 1, [0x09] = 2, [0x0a] = 2, [0x0b] = 3,
@@ -73,7 +73,7 @@ export const uint8_t popcount_table[256] = {
     [0xfc] = 6, [0xfd] = 7, [0xfe] = 7, [0xff] = 8,
 };
 
-export const uint8_t popparity_table[256] = {
+export const uint8_t bfdev_popparity_table[256] = {
     [0x00] = 0, [0x01] = 1, [0x02] = 1, [0x03] = 0,
     [0x04] = 1, [0x05] = 0, [0x06] = 0, [0x07] = 1,
     [0x08] = 1, [0x09] = 0, [0x0a] = 0, [0x0b] = 1,
