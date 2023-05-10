c82cee22a16be34a275a1f5303b4f6e82e84f3f4
John Sanpe <sanpeqf@gmail.com>
2023-05-11 01:07:59 +0800

    feat version: added version declaration in config
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

---
+++ scripts/commit.cmake
@@ -0,0 +1,32 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# Copyright(c) 2023 John Sanpe <sanpeqf@gmail.com>
+#
+
+macro(commit_hash hash)
+    find_package(Git QUIET)
+    if(GIT_FOUND)
+        execute_process(
+            COMMAND ${GIT_EXECUTABLE} rev-parse -q HEAD
+            OUTPUT_VARIABLE ${hash}
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+            ERROR_QUIET
+            WORKING_DIRECTORY
+            ${CMAKE_CURRENT_SOURCE_DIR}
+        )
+    endif(GIT_FOUND)
+endmacro(commit_hash)
+
+macro(commit_branch branch)
+    find_package(Git QUIET)
+    if(GIT_FOUND)
+        execute_process(
+            COMMAND ${GIT_EXECUTABLE} symbolic-ref --short -q HEAD
+            OUTPUT_VARIABLE ${branch}
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+            ERROR_QUIET
+            WORKING_DIRECTORY
+            ${CMAKE_CURRENT_SOURCE_DIR}
+        )
+    endif(GIT_FOUND)
+endmacro(commit_branch)
