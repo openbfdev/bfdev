b4f0a129f783016b6c1aa0ef9adc00667f57db06
John Sanpe <sanpeqf@gmail.com>
2023-05-11 01:12:51 +0800

    rename bcd: batch added bfdev prefix
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- include/bfdev/bcd.h
+++ include/bfdev/bcd.h
@@ -15,49 +15,49 @@
 extern "C" {
 #endif
 
-#define bcd2bin_constant(bcd) ( \
+#define bfdev_bcd2bin_constant(bcd) ( \
     ((bcd) & 0x0f) + ((bcd) >> 4) * 10 \
 )
 
-#define bin2bcd_constant(bin) ( \
+#define bfdev_bin2bcd_constant(bin) ( \
     (((bin) / 10) << 4) + (bin) % 10 \
 )
 
-extern const uint8_t bcd2bin_table[256];
-extern const uint8_t bin2bcd_table[256];
+extern const uint8_t bfdev_bcd2bin_table[256];
+extern const uint8_t bfdev_bin2bcd_table[256];
 
 static __attribute_const__ inline
-uint8_t bcd2bin_dynamic(uint8_t bcd)
+uint8_t bfdev_bcd2bin_dynamic(uint8_t bcd)
 {
-    return bcd2bin_table[bcd];
+    return bfdev_bcd2bin_table[bcd];
 }
 
 static inline __attribute_const__
-uint8_t bin2bcd_dynamic(uint8_t bin)
+uint8_t bfdev_bin2bcd_dynamic(uint8_t bin)
 {
-    return bin2bcd_table[bin];
+    return bfdev_bin2bcd_table[bin];
 }
 
 /**
  * bcd2bin - convert bcd to bin.
  * @bcd: the bcd to convert.
  */
-#define bcd2bin(bcd) ({                 \
+#define bfdev_bcd2bin(bcd) ({           \
     uint8_t __bcd = (uint8_t)(bcd);     \
     __builtin_constant_p(__bcd)         \
-    ? bcd2bin_constant(__bcd)           \
-    : bcd2bin_dynamic(__bcd);           \
+    ? bfdev_bcd2bin_constant(__bcd)     \
+    : bfdev_bcd2bin_dynamic(__bcd);     \
 })
 
 /**
  * bin2bcd - convert bin to bcd.
  * @bcd: the bin to convert.
  */
-#define bin2bcd(bin) ({                 \
+#define bfdev_bin2bcd(bin) ({           \
     uint8_t __bin = (uint8_t)(bin);     \
     __builtin_constant_p(__bin)         \
-    ? bin2bcd_constant(__bin)           \
-    : bin2bcd_dynamic(__bin);           \
+    ? bfdev_bin2bcd_constant(__bin)     \
+    : bfdev_bin2bcd_dynamic(__bin);     \
 })
 
 #ifdef __cplusplus
