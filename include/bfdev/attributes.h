ed9d921aaab4dd991d910cb3d81b2140ba81d019
John Sanpe <sanpeqf@gmail.com>
2023-05-14 07:38:56 +0000

    fixup attributes: mandatory declaration macro
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- include/bfdev/attributes.h
+++ include/bfdev/attributes.h
@@ -48,19 +48,19 @@ extern "C" {
 /*
  *   gcc: https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html#index-alias-function-attribute
  */
-#ifndef __alias
-# define __alias(symbol) __attribute__((__alias__(symbol)))
-#endif
+#undef __alias
+#define __alias(symbol) __attribute__((__alias__(symbol)))
 
 /*
  *   gcc: https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html#index-aligned-function-attribute
  *   gcc: https://gcc.gnu.org/onlinedocs/gcc/Common-Type-Attributes.html#index-aligned-type-attribute
  *   gcc: https://gcc.gnu.org/onlinedocs/gcc/Common-Variable-Attributes.html#index-aligned-variable-attribute
  */
-#ifndef __aligned
-# define __aligned(x) __attribute__((__aligned__(x)))
-# define __aligned_largest __attribute__((__aligned__))
-#endif
+#undef __aligned
+#define __aligned(x) __attribute__((__aligned__(x)))
+
+#undef __aligned_largest
+#define __aligned_largest __attribute__((__aligned__))
 
 /*
  * Note: users of __always_inline currently do not write "inline" themselves,
@@ -71,9 +71,8 @@ extern "C" {
  *   gcc: https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html#index-always_005finline-function-attribute
  * clang: mentioned
  */
-#ifndef __always_inline
-# define __always_inline inline __attribute__((__always_inline__))
-#endif
+#undef __always_inline
+#define __always_inline inline __attribute__((__always_inline__))
 
 /*
  * The second argument is optional (default 0), so we use a variadic macro
@@ -91,30 +90,27 @@ extern "C" {
  *   gcc: https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html#index-assume_005faligned-function-attribute
  * clang: https://clang.llvm.org/docs/AttributeReference.html#assume-aligned
  */
-#ifndef __assume_aligned
-# if __has_attribute(__assume_aligned__)
-#  define __assume_aligned(a, ...) __attribute__((__assume_aligned__(a, ## __VA_ARGS__)))
-# else
-#  define __assume_aligned(a, ...)
-# endif
+#undef __assume_aligned
+#if __has_attribute(__assume_aligned__)
+# define __assume_aligned(a, ...) __attribute__((__assume_aligned__(a, ## __VA_ARGS__)))
+#else
+# define __assume_aligned(a, ...)
 #endif
 
 /*
  *   gcc: https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html#index-cold-function-attribute
  *   gcc: https://gcc.gnu.org/onlinedocs/gcc/Label-Attributes.html#index-cold-label-attribute
  */
-#ifndef __cold
-# define __cold __attribute__((__cold__))
-#endif
+#undef __cold
+#define __cold __attribute__((__cold__))
 
 /*
  * Note the long name.
  *
  *   gcc: https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html#index-const-function-attribute
  */
-#ifndef __attribute_const__
-# define __attribute_const__ __attribute__((__const__))
-#endif
+#undef __attribute_const__
+#define __attribute_const__ __attribute__((__const__))
 
 /*
  * Optional: only supported since gcc >= 9
@@ -123,12 +119,11 @@ extern "C" {
  *
  *   gcc: https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html#index-copy-function-attribute
  */
-#ifndef __copy
-# if __has_attribute(__copy__)
-#  define __copy(symbol) __attribute__((__copy__(symbol)))
-# else
-#  define __copy(symbol)
-# endif
+#undef __copy
+#if __has_attribute(__copy__)
+# define __copy(symbol) __attribute__((__copy__(symbol)))
+#else
+# define __copy(symbol)
 #endif
 
 /*
@@ -141,9 +136,8 @@ extern "C" {
  *   gcc: https://gcc.gnu.org/onlinedocs/gcc/Enumerator-Attributes.html#index-deprecated-enumerator-attribute
  * clang: https://clang.llvm.org/docs/AttributeReference.html#deprecated
  */
-#ifndef __deprecated
-# define __deprecated
-#endif
+#undef __deprecated
+#define __deprecated
 
 /*
  * Optional: only supported since gcc >= 5.1
@@ -152,12 +146,11 @@ extern "C" {
  *
  *   gcc: https://gcc.gnu.org/onlinedocs/gcc/Common-Type-Attributes.html#index-designated_005finit-type-attribute
  */
-#ifndef __designated_init
-# if __has_attribute(__designated_init__)
-#  define __designated_init __attribute__((__designated_init__))
-# else
-#  define __designated_init
-# endif
+#undef __designated_init
+#if __has_attribute(__designated_init__)
+# define __designated_init __attribute__((__designated_init__))
+#else
+# define __designated_init
 #endif
 
 /*
@@ -165,53 +158,49 @@ extern "C" {
  *
  *   gcc: https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html#index-externally_005fvisible-function-attribute
  */
-#ifndef __visible
-# if __has_attribute(__externally_visible__)
-#  define __visible __attribute__((__externally_visible__))
-# else
-#  define __visible
-# endif
+#undef __visible
+#if __has_attribute(__externally_visible__)
+# define __visible __attribute__((__externally_visible__))
+#else
+# define __visible
 #endif
 
 /*
  *   gcc: https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html#index-format-function-attribute
  * clang: https://clang.llvm.org/docs/AttributeReference.html#format
  */
-#ifndef __printf
-# define __printf(a, b) __attribute__((__format__(printf, a, b)))
-# define __scanf(a, b) __attribute__((__format__(scanf, a, b)))
-#endif
+#undef __printf
+#define __printf(a, b) __attribute__((__format__(printf, a, b)))
+
+#undef __scanf
+#define __scanf(a, b) __attribute__((__format__(scanf, a, b)))
 
 /*
  *   gcc: https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html#index-nonnull-function-attribute
  * clang: https://clang.llvm.org/docs/AttributeReference.html#nonnull
  */
-#ifndef __nonnull
-# define __nonnull(nr, ...) __attribute__((__nonnull__(nr, ##__VA_ARGS__)))
-#endif
+#undef __nonnull
+#define __nonnull(nr, ...) __attribute__((__nonnull__(nr, ##__VA_ARGS__)))
 
 /*
  *   gcc: https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html#index-gnu_005finline-function-attribute
  * clang: https://clang.llvm.org/docs/AttributeReference.html#gnu-inline
  */
-#ifndef __gnu_inline
-# define __gnu_inline __attribute__((__gnu_inline__))
-#endif
+#undef __gnu_inline
+#define __gnu_inline __attribute__((__gnu_inline__))
 
 /*
  *   gcc: https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html#index-malloc-function-attribute
  */
-#ifndef __malloc
-# define __malloc __attribute__((__malloc__))
-#endif
+#undef __malloc
+#define __malloc __attribute__((__malloc__))
 
 /*
  *   gcc: https://gcc.gnu.org/onlinedocs/gcc/Common-Type-Attributes.html#index-mode-type-attribute
  *   gcc: https://gcc.gnu.org/onlinedocs/gcc/Common-Variable-Attributes.html#index-mode-variable-attribute
  */
-#ifndef __mode
-# define __mode(x) __attribute__((__mode__(x)))
-#endif
+#undef __mode
+#define __mode(x) __attribute__((__mode__(x)))
 
 /*
  * Optional: only supported since gcc >= 7
@@ -219,12 +208,11 @@ extern "C" {
  *   gcc: https://gcc.gnu.org/onlinedocs/gcc/x86-Function-Attributes.html#index-no_005fcaller_005fsaved_005fregisters-function-attribute_002c-x86
  * clang: https://clang.llvm.org/docs/AttributeReference.html#no-caller-saved-registers
  */
-#ifndef __no_caller_saved_registers
-# if __has_attribute(__no_caller_saved_registers__)
-#  define __no_caller_saved_registers __attribute__((__no_caller_saved_registers__))
-# else
-#  define __no_caller_saved_registers
-# endif
+#undef __no_caller_saved_registers
+#if __has_attribute(__no_caller_saved_registers__)
+# define __no_caller_saved_registers __attribute__((__no_caller_saved_registers__))
+#else
+# define __no_caller_saved_registers
 #endif
 
 /*
@@ -232,12 +220,11 @@ extern "C" {
  *
  *  gcc: https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html#index-noclone-function-attribute
  */
-#ifndef __noclone
-# if __has_attribute(__noclone__)
-#  define __noclone __attribute__((__noclone__))
-# else
-#  define __noclone
-# endif
+#undef __noclone
+#if __has_attribute(__noclone__)
+# define __noclone __attribute__((__noclone__))
+#else
+# define __noclone
 #endif
 
 /*
@@ -250,12 +237,11 @@ extern "C" {
  *
  *  gcc: https://gcc.gnu.org/onlinedocs/gcc/Statement-Attributes.html#Statement-Attributes
  */
-#ifndef fallthrough
-# if __has_attribute(__fallthrough__)
-#  define fallthrough __attribute__((__fallthrough__))
-# else
-#  define fallthrough do {} while (0)  /* fallthrough */
-# endif
+#undef fallthrough
+#if __has_attribute(__fallthrough__)
+# define fallthrough __attribute__((__fallthrough__))
+#else
+# define fallthrough do {} while (0)  /* fallthrough */
 #endif
 
 /*
@@ -264,9 +250,8 @@ extern "C" {
  *   gcc: https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html#index-noinline-function-attribute
  * clang: mentioned
  */
-#ifndef noinline
-# define noinline __attribute__((__noinline__))
-#endif
+#undef noinline
+#define noinline __attribute__((__noinline__))
 
 /*
  * Optional: only supported since gcc >= 8
@@ -275,23 +260,21 @@ extern "C" {
  *
  *   gcc: https://gcc.gnu.org/onlinedocs/gcc/Common-Variable-Attributes.html#index-nonstring-variable-attribute
  */
-#ifndef __nonstring
-# if __has_attribute(__nonstring__)
-#  define __nonstring __attribute__((__nonstring__))
-# else
-#  define __nonstring
-# endif
+#undef __nonstring
+#if __has_attribute(__nonstring__)
+# define __nonstring __attribute__((__nonstring__))
+#else
+# define __nonstring
 #endif
 
 /*
  *   gcc: https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html#index-functions-that-return-more-than-once
  */
-#ifndef __returns_twice
-# if __has_attribute(__returns_twice__)
-#  define __returns_twice __attribute__((__returns_twice__))
-# else
-#  define __returns_twice
-# endif
+#undef __returns_twice
+#if __has_attribute(__returns_twice__)
+# define __returns_twice __attribute__((__returns_twice__))
+#else
+# define __returns_twice
 #endif
 
 /*
@@ -299,33 +282,29 @@ extern "C" {
  * clang: https://clang.llvm.org/docs/AttributeReference.html#noreturn
  * clang: https://clang.llvm.org/docs/AttributeReference.html#id1
  */
-#ifndef __noreturn
-# define __noreturn __attribute__((__noreturn__))
-#endif
+#undef __noreturn
+#define __noreturn __attribute__((__noreturn__))
 
 /*
  *   gcc: https://gcc.gnu.org/onlinedocs/gcc/Common-Type-Attributes.html#index-packed-type-attribute
  * clang: https://gcc.gnu.org/onlinedocs/gcc/Common-Variable-Attributes.html#index-packed-variable-attribute
  */
-#ifndef __packed
-# define __packed __attribute__((__packed__))
-#endif
+#undef __packed
+#define __packed __attribute__((__packed__))
 
 /*
  *   gcc: https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html#index-pure-function-attribute
  */
-#ifndef __pure
-# define __pure __attribute__((__pure__))
-#endif
+#undef __pure
+#define __pure __attribute__((__pure__))
 
 /*
  *   gcc: https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html#index-section-function-attribute
  *   gcc: https://gcc.gnu.org/onlinedocs/gcc/Common-Variable-Attributes.html#index-section-variable-attribute
  * clang: https://clang.llvm.org/docs/AttributeReference.html#section-declspec-allocate
  */
-#ifndef __section
-# define __section(section) __attribute__((__section__(section)))
-#endif
+#undef __section
+#define __section(section) __attribute__((__section__(section)))
 
 /*
  *   gcc: https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html#index-unused-function-attribute
@@ -334,47 +313,43 @@ extern "C" {
  *   gcc: https://gcc.gnu.org/onlinedocs/gcc/Label-Attributes.html#index-unused-label-attribute
  * clang: https://clang.llvm.org/docs/AttributeReference.html#maybe-unused-unused
  */
-#ifndef __always_unused
-# define __always_unused __attribute__((__unused__))
-# define __maybe_unused __attribute__((__unused__))
-#endif
+#undef __always_unused
+#define __always_unused __attribute__((__unused__))
+
+#undef __always_unused
+#define __maybe_unused __attribute__((__unused__))
 
 /*
  *   gcc: https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html#index-used-function-attribute
  *   gcc: https://gcc.gnu.org/onlinedocs/gcc/Common-Variable-Attributes.html#index-used-variable-attribute
  */
-#ifndef __used
-# define __used __attribute__((__used__))
-#endif
+#undef __used
+#define __used __attribute__((__used__))
 
 /*
  *   gcc: https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html#index-weak-function-attribute
  *   gcc: https://gcc.gnu.org/onlinedocs/gcc/Common-Variable-Attributes.html#index-weak-variable-attribute
  */
-#ifndef __weak
-# define __weak __attribute__((__weak__))
-#endif
+#undef __weak
+#define __weak __attribute__((__weak__))
 
 /*
  *   gcc: https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html#index-visibility-function-attribute
  */
-#ifndef __visibility
-# define __visibility(mode) __attribute__((visibility(mode)))
-#endif
+#undef __visibility
+#define __visibility(mode) __attribute__((visibility(mode)))
 
 /*
  *   gcc: https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html#index-constructor-function-attribute
  */
-#ifndef __ctor
-# define __ctor __attribute__((constructor))
-#endif
+#undef __ctor
+#define __ctor __attribute__((constructor))
 
 /*
  *   gcc: https://gcc.gnu.org/onlinedocs/gcc/Common-Function-Attributes.html#index-destructor-function-attribute
  */
-#ifndef __dtor
-# define __dtor __attribute__((destructor))
-#endif
+#undef __dtor
+#define __dtor __attribute__((destructor))
 
 #ifdef __cplusplus
 }
