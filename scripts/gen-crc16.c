7a9bd62a581cbf182ad12018426a6dcc3bfada78
John Sanpe <sanpeqf@gmail.com>
2023-06-11 14:37:00 +0800

    optimize crc: optimize multiple le algorithms
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- scripts/gen-crc16.c
+++ scripts/gen-crc16.c
@@ -3,9 +3,9 @@
  * Copyright(c) 2023 John Sanpe <sanpeqf@gmail.com>
  */
 
-#define GENCRC_NAME crc32
-#define GENCRC_TYPE uint32_t
-#define GENCRC_BITS 32
-#define GENCRC_WIDE 8
+#define GENCRC_NAME crc16
+#define GENCRC_TYPE uint16_t
+#define GENCRC_BITS 16
+#define GENCRC_WIDE 4
 #define GENCRC_BELE 1
 #include "gen-crc.c"
