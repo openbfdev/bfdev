45cbd3df10ca422ec09cad89ac83f029a5989e76
John Sanpe <sanpeqf@gmail.com>
2023-06-16 23:53:39 +0800

    feat stringhash: added initial pjwhash function
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

---
+++ include/bfdev/stringhash.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright(c) 2023 John Sanpe <sanpeqf@gmail.com>
+ */
+
+#ifndef _BFDEV_STRINGHASH_H_
+#define _BFDEV_STRINGHASH_H_
+
+#include <bfdev/config.h>
+#include <bfdev/cdefs.h>
+
+#define BFDEV_PJWHASH_BITS 32
+
+extern unsigned long bfdev_pjwhash(const char *str);
+
+#endif /* _BFDEV_STRINGHASH_H_ */
