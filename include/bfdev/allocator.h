6f1978765402a276ddc93d426d49717a2906f6b4
John Sanpe <sanpeqf@gmail.com>
2023-06-21 17:07:57 +0800

    feat allocator: added realloc/malloc array functions
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- include/bfdev/allocator.h
+++ include/bfdev/allocator.h
@@ -25,6 +25,31 @@ extern __bfdev_malloc void *bfdev_zalloc(const struct bfdev_alloc *alloc, size_t
 extern __bfdev_malloc void *bfdev_realloc(const struct bfdev_alloc *alloc, const void *block, size_t resize);
 extern void bfdev_free(const struct bfdev_alloc *alloc, const void *block);
 
+/**
+ * bfdev_malloc_array - allocate memory for an array.
+ * @nr: number of elements.
+ * @size: single element size.
+ */
+static __bfdev_always_inline __bfdev_malloc void *
+bfdev_malloc_array(const struct bfdev_alloc *alloc,
+                   size_t nr, size_t size)
+{
+    return bfdev_malloc(alloc, size * nr);
+}
+
+/**
+ * bfdev_realloc_array - reallocate memory for an array.
+ * @block: number of elements.
+ * @nr: number of elements.
+ * @size: single element size.
+ */
+static __bfdev_always_inline __bfdev_malloc void *
+bfdev_realloc_array(const struct bfdev_alloc *alloc,
+                    void *block, size_t nr, size_t size)
+{
+    return bfdev_realloc(alloc, block, size * nr);
+}
+
 BFDEV_END_DECLS
 
 #endif /* _BFDEV_ALLOCATOR_H_ */
