9f8c8074077b9bd98212e4f6d42b4dd2821e2c26
John Sanpe <sanpeqf@gmail.com>
2023-06-22 12:42:01 +0800

    feat ringbuf: added simple example
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

---
+++ examples/ringbuf/simple.c
@@ -0,0 +1,50 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright(c) 2022 Sanpe <sanpeqf@gmail.com>
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <bfdev/ringbuf.h>
+
+static const char test_table[32] = {
+    'b', 'f', 'd', 'e', 'v', '-', 'r', 'i',
+    'n', 'g', '-', 't', 'e', 's', 't', '\n',
+    'b', 'f', 'd', 'e', 'v', '-', 'r', 'i',
+    'n', 'g', '-', 't', 'e', 's', 't', '\n',
+};
+
+static const char warn_table[16] = {
+    '=', '=', '=', '=', '=', '=', '=', '=',
+    '=', '=', '=', '=', '=', '=', '=', '\n',
+};
+
+#define TEST_SIZE 16
+#define TEST_LOOP 64
+BFDEV_DEFINE_RINGBUF(normal_bytetest, char, TEST_SIZE);
+
+static void ringbuf_warnup(void)
+{
+    unsigned int count;
+
+    bfdev_ringbuf_homing(&normal_bytetest);
+    for (count = 0; count < TEST_SIZE; ++count)
+        bfdev_ringbuf_put(&normal_bytetest, warn_table[count]);
+}
+
+int main(void)
+{
+    unsigned int count, index;
+    char ch;
+
+    ringbuf_warnup();
+    for (count = 0; count < TEST_LOOP; ++count) {
+        bfdev_ringbuf_peek(&normal_bytetest, &ch);
+        printf("%c", ch);
+        index = count % TEST_SIZE;
+        bfdev_ringbuf_put(&normal_bytetest, test_table[index]);
+    }
+
+    return 0;
+}
