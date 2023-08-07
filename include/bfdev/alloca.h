22cdde7c2cf0aab71c24d4cadeca7b59f566d3c7
John Sanpe <sanpeqf@gmail.com>
2023-08-08 02:58:45 +0800

    feat alloca: added initial functions
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

---
+++ include/bfdev/alloca.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright(c) 2023 John Sanpe <sanpeqf@gmail.com>
+ */
+
+#ifndef _BFDEV_ALLOCA_H_
+#define _BFDEV_ALLOCA_H_
+
+#include <bfdev/config.h>
+#include <bfdev/stdint.h>
+
+BFDEV_BEGIN_DECLS
+
+#define bfdev_alloca __builtin_alloca
+extern __bfdev_malloc void *bfdev_alloca(unsigned int size);
+
+BFDEV_END_DECLS
+
+#endif /* _BFDEV_ALLOCA_H_ */
