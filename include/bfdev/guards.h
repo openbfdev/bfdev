544f5495ada80804cedef39f510acc58ed2d1d35
John Sanpe <sanpeqf@gmail.com>
2023-08-10 10:41:52 +0800

    feat guards: added support for cleanup garbage-collection
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

---
+++ include/bfdev/guards.h
@@ -0,0 +1,35 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright(c) 2023 John Sanpe <sanpeqf@gmail.com>
+ */
+
+#ifndef _BFDEV_GUARDS_H_
+#define _BFDEV_GUARDS_H_
+
+#include <bfdev/config.h>
+#include <bfdev/attributes.h>
+
+BFDEV_BEGIN_DECLS
+
+#define BFDEV_CLEAN_TEMPLATE(name, type, free)  \
+static inline void                              \
+__bfdev_cleanup_##name(void *object)            \
+{                                               \
+    type _T = *(type *)object;                  \
+    free;                                       \
+}
+
+#define bfdev_clean_lasting(object) ({  \
+    __auto_type __ptr = (object);       \
+    (object) = NULL; __ptr;             \
+})
+
+#define bfdev_clean(name) \
+    __bfdev_cleanup(__bfdev_cleanup_##name)
+
+#define bfdev_clean_return(object) \
+    return bfdev_clean_lasting(object)
+
+BFDEV_END_DECLS
+
+#endif /* _BFDEV_GUARDS_H_ */
