a9cd7b1edac008368500bd8f6a51a2274882e17a
John Sanpe <sanpeqf@gmail.com>
2023-08-23 02:54:59 +0800

    fixup fsm: precise return value representation
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- src/fsm.c
+++ src/fsm.c
@@ -14,6 +14,14 @@ fsm_push_state(struct bfdev_fsm *fsm, struct bfdev_fsm_state *state)
     fsm->state[count & (BFDEV_ARRAY_SIZE(fsm->state) - 1)] = state;
 }
 
+static inline bool
+fsm_trans_exist(struct bfdev_fsm_state *state, unsigned int index)
+{
+    if (state->trans[index].next)
+        return true;
+    return state->trans[index].stack < 0;
+}
+
 static struct bfdev_fsm_transition *
 fsm_find_transition(struct bfdev_fsm_state *state, struct bfdev_fsm_event *event)
 {
@@ -23,7 +31,7 @@ fsm_find_transition(struct bfdev_fsm_state *state, struct bfdev_fsm_event *event
     if (!state->trans)
         return NULL;
 
-    for (count = 0; state->trans[count].next || state->trans[count].stack; ++count) {
+    for (count = 0; fsm_trans_exist(state, count); ++count) {
         find = &state->trans[count];
 
         if (find->type != event->type)
@@ -40,48 +48,49 @@ export int
 bfdev_fsm_handle(struct bfdev_fsm *fsm, struct bfdev_fsm_event *event)
 {
     struct bfdev_fsm_state *curr = bfdev_fsm_curr(fsm);
-    struct bfdev_fsm_state *next = curr;
+    struct bfdev_fsm_state *next;
 
     if (unlikely(!curr))
         return -BFDEV_EINVAL;
 
-    do {
+    for (next = curr; next; curr = next) {
         struct bfdev_fsm_transition *tran;
         struct bfdev_fsm_state **pstate;
         int retval;
 
-        tran = fsm_find_transition(next, event);
+        tran = fsm_find_transition(curr, event);
         if (!tran) {
-            next = next->parent;
+            next = curr->parent;
             continue;
         }
 
-        if ((next = tran->next)) {
-            while (next->entry)
-                next = next->entry;
-        } else {
+        if (!(next = tran->next)) {
             pstate = bfdev_array_pop(&fsm->stack, -tran->stack);
             next = *pstate;
         }
 
-        if (next != curr) {
-            if (curr->exit) {
-                retval = curr->exit(event, curr->data);
-                if (unlikely(retval))
-                    return retval;
-            }
+        if (unlikely(!next))
+            next = fsm->error;
 
-            if (tran->action) {
-                retval = tran->action(event, tran->data, curr, next);
-                if (unlikely(retval))
-                    return retval;
-            }
+        while (next->entry)
+            next = next->entry;
 
-            if (next->enter) {
-                retval = curr->enter(event, next->data);
-                if (unlikely(retval))
-                    return retval;
-            }
+        if (next != curr && curr->exit) {
+            retval = curr->exit(event, curr->data);
+            if (unlikely(retval))
+                return retval;
+        }
+
+        if (tran->action) {
+            retval = tran->action(event, tran->data, curr, next);
+            if (unlikely(retval))
+                return retval;
+        }
+
+        if (next != curr && next->enter) {
+            retval = curr->enter(event, next->data);
+            if (unlikely(retval))
+                return retval;
         }
 
         if (tran->stack > 0) {
@@ -92,17 +101,20 @@ bfdev_fsm_handle(struct bfdev_fsm *fsm, struct bfdev_fsm_event *event)
         }
 
         fsm_push_state(fsm, next);
+        if (unlikely(next == fsm->error))
+            return -BFDEV_EFAULT;
+
         if (tran->cross)
             continue;
 
         if (unlikely(curr == next))
-            return -BFDEV_EDEADLK;
+            return BFDEV_FSM_SELFLOOP;
 
-        if (unlikely(next == fsm->error))
-            return -BFDEV_EFAULT;
+        if (!fsm_trans_exist(next, 0))
+            return BFDEV_FSM_FINISH;
 
-        return -BFDEV_ENOERR;
-    } while ((curr = next));
+        return BFDEV_FSM_CHANGED;
+    }
 
-    return -BFDEV_ENOENT;
+    return BFDEV_FSM_NOCHANGE;
 }
