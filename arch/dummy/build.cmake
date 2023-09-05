a9de15c10c845f73d68c238e886180fc745e6110
John Sanpe <sanpeqf@gmail.com>
2023-09-05 09:09:19 +0800

    build files: manually adding instead of automatic scanning
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

---
+++ arch/dummy/build.cmake
@@ -0,0 +1,8 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# Copyright(c) 2023 John Sanpe <sanpeqf@gmail.com>
+#
+
+#
+# Nothing
+#
---
+++ src/textsearch/build.cmake
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# Copyright(c) 2023 John Sanpe <sanpeqf@gmail.com>
+#
+
+set(BFDEV_SOURCE
+    ${BFDEV_SOURCE}
+    ${CMAKE_CURRENT_LIST_DIR}/bm.c
+    ${CMAKE_CURRENT_LIST_DIR}/kmp.c
+    ${CMAKE_CURRENT_LIST_DIR}/linear.c
+    ${CMAKE_CURRENT_LIST_DIR}/sunday.c
+    ${CMAKE_CURRENT_LIST_DIR}/textsearch.c
+)
