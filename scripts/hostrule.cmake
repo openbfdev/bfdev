9cd1b443f859ff4511e3419cee2574e9f1fc4c22
John Sanpe <sanpeqf@gmail.com>
2023-09-10 19:50:00 +0800

    build hostrule: fixed compilation issues
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- scripts/hostrule.cmake
+++ scripts/hostrule.cmake
@@ -3,17 +3,16 @@
 # Copyright(c) 2023 John Sanpe <sanpeqf@gmail.com>
 #
 
-if(NOT HOST_C_COMPILER)
-    set(HOST_C_COMPILER ${CMAKE_C_COMPILER})
-    set(HOST_C_FLAGS ${CMAKE_C_FLAGS})
-endif()
-
 macro(host_target target source)
-    add_custom_target(${target}
-        COMMAND
-        ${HOST_C_COMPILER} ${HOST_C_FLAGS}
-        -o ${CMAKE_CURRENT_BINARY_DIR}/${target} ${source}
-        WORKING_DIRECTORY
-        ${CMAKE_CURRENT_SOURCE_DIR}
-    )
+    if(NOT HOST_C_COMPILER)
+        add_executable(${target} ${source})
+    else()
+        add_custom_target(${target}
+            COMMAND
+            ${HOST_C_COMPILER} ${HOST_C_FLAGS}
+            -o ${CMAKE_CURRENT_BINARY_DIR}/${target} ${source}
+            WORKING_DIRECTORY
+            ${CMAKE_CURRENT_SOURCE_DIR}
+        )
+    endif()
 endmacro()
