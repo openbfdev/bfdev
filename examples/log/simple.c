a26a6196b9a876f40cfa344ea4018487ead5b62a
John Sanpe <sanpeqf@gmail.com>
2023-09-14 20:01:28 +0800

    feat log: added simple example
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

---
+++ examples/log/simple.c
@@ -0,0 +1,24 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright(c) 2023 John Sanpe <sanpeqf@gmail.com>
+ */
+
+#define MODULE_NAME "log-simple"
+#define bfdev_log_fmt(fmt) MODULE_NAME ": " fmt
+
+#include <bfdev/log.h>
+
+int main(void)
+{
+    bfdev_log_emerg("helloworld\n");
+    bfdev_log_alert("helloworld\n");
+    bfdev_log_crit("helloworld\n");
+    bfdev_log_err("helloworld\n");
+    bfdev_log_warn("helloworld\n");
+    bfdev_log_notice("helloworld\n");
+    bfdev_log_info("helloworld\n");
+    bfdev_log_debug("helloworld\n");
+    bfdev_log_print("helloworld\n");
+
+    return 0;
+}
