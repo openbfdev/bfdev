902c1c17924dcdd3fbea44e718022c8a7d68e2db
John Sanpe <sanpeqf@gmail.com>
2023-09-18 22:45:57 +0800

    feat bfdev: added version example
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

---
+++ examples/bfdev/version.c
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright(c) 2023 Sanpe <sanpeqf@gmail.com>
+ */
+
+#include <stdio.h>
+#include <bfdev/config.h>
+
+int main(void)
+{
+    printf("arch: %s\n", __bfdev_stringify(BFDEV_ARCH));
+    printf("name: %s\n", __bfdev_stringify(BFDEV_NAME));
+    printf("version: %s\n", __bfdev_stringify(BFDEV_VERSION));
+    printf("commitid: %s\n", __bfdev_stringify(BFDEV_COMMITID));
+    printf("branch: %s\n", __bfdev_stringify(BFDEV_BRANCH));
+    return 0;
+}
