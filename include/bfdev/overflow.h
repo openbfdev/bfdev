2e12e7e5c21528ba07c101f378777dadf0975d99
John Sanpe <sanpeqf@gmail.com>
2023-09-22 12:21:49 +0800

    docs overflow: added api comments
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- include/bfdev/overflow.h
+++ include/bfdev/overflow.h
@@ -8,35 +8,63 @@
 
 #include <bfdev/config.h>
 #include <bfdev/types.h>
+#include <bfdev/stdbool.h>
 
 BFDEV_BEGIN_DECLS
 
-#define bfdev_overflow_check_add(a, b, d) ({    \
+/**
+ * bfdev_overflow_check_add() - Calculate addition with overflow checking.
+ * @a: first addend.
+ * @b: second addend.
+ * @d: pointer to store sum.
+ *
+ * Returns 0 on success.
+ */
+#define bfdev_overflow_check_add(a, b, d)       \
+bfdev_overflow_check(({                         \
     typeof(a) ___a = (a);                       \
     typeof(b) ___b = (b);                       \
     typeof(d) ___d = (d);                       \
     (void) (&___a == &___b);                    \
     (void) (&___a == ___d);                     \
     __builtin_add_overflow(___a, ___b, ___d);   \
-})
+}))
 
-#define bfdev_overflow_check_sub(a, b, d) ({    \
+/**
+ * bfdev_overflow_check_sub() - Calculate subtraction with overflow checking.
+ * @a: minuend; value to subtract from.
+ * @b: subtrahend; value to subtract from @a.
+ * @d: pointer to store difference.
+ *
+ * Returns 0 on success.
+ */
+#define bfdev_overflow_check_sub(a, b, d)       \
+bfdev_overflow_check(({                         \
     typeof(a) ___a = (a);                       \
     typeof(b) ___b = (b);                       \
     typeof(d) ___d = (d);                       \
     (void) (&___a == &___b);                    \
     (void) (&___a == ___d);                     \
     __builtin_sub_overflow(___a, ___b, ___d);   \
-})
+}))
 
-#define bfdev_overflow_check_mul(a, b, d) ({    \
+/**
+ * bfdev_overflow_check_mul() - Calculate multiplication with overflow checking.
+ * @a: first factor.
+ * @b: second factor.
+ * @d: pointer to store product.
+ *
+ * Returns 0 on success.
+ */
+#define bfdev_overflow_check_mul(a, b, d)       \
+bfdev_overflow_check(({                         \
     typeof(a) ___a = (a);                       \
     typeof(b) ___b = (b);                       \
     typeof(d) ___d = (d);                       \
     (void) (&___a == &___b);                    \
     (void) (&___a == ___d);                     \
     __builtin_mul_overflow(___a, ___b, ___d);   \
-})
+}))
 
 #define bfdev_overflow_add_type(type, a, b) ({  \
     type __a = (type)(a);                       \
@@ -62,9 +90,41 @@ BFDEV_BEGIN_DECLS
     ? (type)~0ULL : __d;                        \
 })
 
-#define bfdev_overflow_add(a, b) bfdev_overflow_add_type(typeof(a), a, b)
-#define bfdev_overflow_sub(a, b) bfdev_overflow_sub_type(typeof(a), a, b)
-#define bfdev_overflow_mul(a, b) bfdev_overflow_mul_type(typeof(a), a, b)
+static inline __bfdev_must_check bool
+bfdev_overflow_check(bool overflow)
+{
+    return bfdev_unlikely(overflow);
+}
+
+/**
+ * bfdev_overflow_add() - Calculate type addition with saturation.
+ * @a: first addend.
+ * @b: second addend.
+ *
+ * Returns (type)~0ULL on failed.
+ */
+#define bfdev_overflow_add(a, b) \
+    bfdev_overflow_add_type(typeof(a), a, b)
+
+/**
+ * bfdev_overflow_sub() - Calculate type subtraction with saturation.
+ * @a: minuend; value to subtract from.
+ * @b: subtrahend; value to subtract from @a.
+ *
+ * Returns (type)~0ULL on failed.
+ */
+#define bfdev_overflow_sub(a, b) \
+    bfdev_overflow_sub_type(typeof(a), a, b)
+
+/**
+ * bfdev_overflow_mul() - Calculate type multiplication with saturation.
+ * @a: first factor.
+ * @b: second factor.
+ *
+ * Returns (type)~0ULL on failed.
+ */
+#define bfdev_overflow_mul(a, b) \
+    bfdev_overflow_mul_type(typeof(a), a, b)
 
 #define BFDEV_GENERIC_OVERFLOW_OPS(name, type)      \
 static __bfdev_always_inline type                   \
