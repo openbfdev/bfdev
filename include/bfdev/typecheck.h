c0cbc6e0d524173dc6c15b5d53c208962298b3a1
John Sanpe <sanpeqf@gmail.com>
2023-09-24 11:52:18 +0800

    docs typecheck: added api comments
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- include/bfdev/typecheck.h
+++ include/bfdev/typecheck.h
@@ -10,6 +10,14 @@
 
 BFDEV_BEGIN_DECLS
 
+/**
+ * bfdev_typecheck() - Check object type at compile time.
+ * @type: types to comparison.
+ * @object: the object to check.
+ *
+ * Check at compile time that something is of a particular type.
+ * Always evaluates to 1 so you may use it easily in comparisons.
+ */
 #define bfdev_typecheck(type, object) ({    \
     type __dummy1;                          \
     typeof(object) __dummy2;                \
@@ -17,12 +25,26 @@ BFDEV_BEGIN_DECLS
     1;                                      \
 })
 
+/**
+ * bfdev_typecheck_func() - Check function type at compile time.
+ * @type: types to comparison.
+ * @func: the func to check.
+ *
+ * Check at compile time that 'function' is a certain type, or is a pointer
+ * to that type (needs to use typedef for the function type.)
+ */
 #define bfdev_typecheck_func(type, func) ({ \
     typeof(type) __dummy = func;            \
     (void)__dummy;                          \
     1;                                      \
 })
 
+/**
+ * bfdev_typecheck_pointer() - Check if object is a pointer at compile time.
+ * @pointer: the pointer to check.
+ *
+ * Check at compile time that something is a pointer type.
+ */
 #define bfdev_typecheck_pointer(pointer) ({ \
     typeof(pointer) __dummy;                \
     (void)sizeof(*__dummy);                 \
