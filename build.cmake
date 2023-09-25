6f9a256e48d8491a0373127909d2d0af4bf52418
John Sanpe <sanpeqf@gmail.com>
2023-09-25 10:19:52 +0800

    build cmake: detach file rules
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

---
+++ build.cmake
@@ -0,0 +1,7 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# Copyright(c) 2023 John Sanpe <sanpeqf@gmail.com>
+#
+
+include(${BFDEV_ARCH_PATH}/build.cmake)
+include(${BFDEV_SOURCE_PATH}/build.cmake)
