d7237e6e0f77b030ad6b659946f01af5c1e98410
John Sanpe <sanpeqf@gmail.com>
2023-10-16 20:29:17 +0800

    fixup matrix: fixed log format typo
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- examples/matrix/benchmark.c
+++ examples/matrix/benchmark.c
@@ -3,7 +3,7 @@
  * Copyright(c) 2023 John Sanpe <sanpeqf@gmail.com>
  */
 
-#define MODULE_NAME "crc-bandwidth"
+#define MODULE_NAME "matrix-benchmark"
 #define bfdev_log_fmt(fmt) MODULE_NAME ": " fmt
 
 #include <stdio.h>
