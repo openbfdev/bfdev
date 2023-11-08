8a6bd26b509a5be76b0537bf3de22c71c5289b31
John Sanpe <sanpeqf@gmail.com>
2023-11-08 13:58:26 +0800

    fixed typo: fixed some global style issues
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- include/bfdev/struct.h
+++ include/bfdev/struct.h
@@ -12,18 +12,18 @@ BFDEV_BEGIN_DECLS
 
 /**
  * bfdev_struct_field - Report the size of a struct field in bytes.
- * @TYPE: The structure containing the field of interest
- * @MEMBER: The field to return the size of
+ * @type: The structure containing the field of interest
+ * @member: The field to return the size of
  */
 #define bfdev_struct_field(TYPE, MEMBER) \
     sizeof((((TYPE *)0)->MEMBER))
 
 /**
  * __bfdev_struct_group - Create a mirrored named and anonyomous struct.
- * @TAG: The tag name for the named sub-struct (usually empty).
- * @NAME: The identifier name of the mirrored sub-struct.
- * @ATTRS: Any struct attributes (usually empty).
- * @MEMBERS: The member declarations for the mirrored structs.
+ * @tag: The tag name for the named sub-struct (usually empty).
+ * @name: The identifier name of the mirrored sub-struct.
+ * @attrs: Any struct attributes (usually empty).
+ * @members: The member declarations for the mirrored structs.
  */
 #define __bfdev_struct_group(TAG, NAME, ATTRS, MEMBERS...) \
     union { \
@@ -41,9 +41,9 @@ BFDEV_BEGIN_DECLS
 
 /**
  * bfdev_struct_group_attr - Create a struct group with trailing attributes.
- * @NAME: The identifier name of the mirrored sub-struct.
- * @ATTRS: Any struct attributes to apply.
- * @MEMBERS: The member declarations for the mirrored structs.
+ * @name: The identifier name of the mirrored sub-struct.
+ * @attrs: Any struct attributes to apply.
+ * @members: The member declarations for the mirrored structs.
  */
 #define bfdev_struct_group_attr(NAME, ATTRS, MEMBERS...) \
     __bfdev_struct_group(/* no tag */, NAME, ATTRS, MEMBERS)
