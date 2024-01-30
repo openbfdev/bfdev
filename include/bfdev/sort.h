f41534d634a142143a3062f939e7e5fd6baadf98
John Sanpe <sanpeqf@gmail.com>
2024-01-30 19:26:32 +0800

    docs sort: added api comments
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- include/bfdev/sort.h
+++ include/bfdev/sort.h
@@ -11,8 +11,20 @@
 
 BFDEV_BEGIN_DECLS
 
+/**
+ * bfdev_sort() - Heap sort an array of elements.
+ * @base: pointer to data to sort.
+ * @num: number of elements.
+ * @cells: size of each element.
+ * @cmp: pointer to comparison function.
+ * @pdata: private data passed to comparison function.
+ *
+ * Sorting time is O(n log n) both on average and worst-case. While
+ * quicksort is slightly faster on average, it suffers from exploitable
+ * O(n^2) worst-case behavior and extra memory requirements.
+ */
 extern int
-bfdev_qsort(void *base, size_t num, size_t cells, bfdev_cmp_t cmp, void *pdata);
+bfdev_sort(void *base, size_t num, size_t cells, bfdev_cmp_t cmp, void *pdata);
 
 BFDEV_END_DECLS
 
