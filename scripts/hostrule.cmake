9900d92ba7d7d44dcf84e72f474fcfbd211e2cee
John Sanpe <sanpeqf@gmail.com>
2024-02-07 01:39:50 +0800

    build cmake: simplify the build script
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- scripts/hostrule.cmake
+++ scripts/hostrule.cmake
@@ -3,14 +3,14 @@
 # Copyright(c) 2023 John Sanpe <sanpeqf@gmail.com>
 #
 
-function(host_target target source)
+function(host_target target)
     if(NOT HOST_C_COMPILER)
-        add_executable(${target} ${source})
+        add_executable(${target} ${ARGN})
     else()
         add_custom_target(${target}
             COMMAND
             ${HOST_C_COMPILER} ${HOST_C_FLAGS}
-            -o ${CMAKE_CURRENT_BINARY_DIR}/${target} ${source}
+            -o ${CMAKE_CURRENT_BINARY_DIR}/${target} ${ARGN}
             WORKING_DIRECTORY
             ${CMAKE_CURRENT_SOURCE_DIR}
         )
