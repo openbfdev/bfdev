d7390b96bdcfb2538b909c9f0bdc525558c8cb14
John Sanpe <sanpeqf@gmail.com>
2024-02-28 17:23:52 +0800

    build cmake: added initial code coverage test
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- build.cmake
+++ build.cmake
@@ -3,6 +3,9 @@
 # Copyright(c) 2023 John Sanpe <sanpeqf@gmail.com>
 #
 
+include_directories(${PROJECT_SOURCE_DIR}/include)
+include_directories(${PROJECT_BINARY_DIR}/generated)
+
 file(GLOB BFDEV_HEADER
     ${BFDEV_HEADER_PATH}/bfdev/*.h
 )
@@ -21,3 +24,20 @@ file(GLOB BFDEV_GENERATED_HEADER
 
 include(${BFDEV_ARCH_PATH}/build.cmake)
 include(${BFDEV_SOURCE_PATH}/build.cmake)
+
+set(BFDEV_LIBRARY_HEADER
+    ${BFDEV_HEADER}
+    ${BFDEV_ASM_HEADER}
+    ${BFDEV_ARCH_ASM_HEADER}
+    ${BFDEV_GENERATED_HEADER}
+)
+
+set(BFDEV_LIBRARY_SOURCE
+    ${BFDEV_SOURCE}
+    ${BFDEV_ARCH_SOURCE}
+)
+
+set(BFDEV_LIBRARY
+    ${BFDEV_LIBRARY_HEADER}
+    ${BFDEV_LIBRARY_SOURCE}
+)
