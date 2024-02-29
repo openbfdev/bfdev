22d1c25c9c653fb7d43420d5dc109e83011eed19
John Sanpe <sanpeqf@gmail.com>
2024-02-29 13:59:08 +0800

    feat prandom: added static declare macro
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- include/bfdev/prandom.h
+++ include/bfdev/prandom.h
@@ -13,12 +13,27 @@
 
 BFDEV_BEGIN_DECLS
 
-typedef struct bfdev_prandom_state bfdev_prandom_state_t;
+#ifndef BFDEV_PRANDOM_SEED
+# define BFDEV_PRANDOM_SEED 0x408c3c09UL
+#endif
 
-struct bfdev_prandom_state {
+typedef struct bfdev_prandom bfdev_prandom_t;
+
+struct bfdev_prandom {
     uint32_t s1, s2;
     uint32_t s3, s4;
 };
+
+#define BFDEV_PRANDOM_STATIC() { \
+    .s1 = BFDEV_PRANDOM_SEED, .s2 = BFDEV_PRANDOM_SEED, \
+    .s3 = BFDEV_PRANDOM_SEED, .s4 = BFDEV_PRANDOM_SEED, \
+}
+
+#define BFDEV_PRANDOM_INIT() \
+    (bfdev_prandom_t) BFDEV_PRANDOM_STATIC()
+
+#define BFDEV_DEFINE_PRANDOM(name) \
+    bfdev_prandom_t name = BFDEV_PRANDOM_INIT()
 
 /* Tausworthe generators */
 #define BFDEV_TAUSWORTHE(seed, a, b, c, d) (    \
@@ -26,30 +41,36 @@ struct bfdev_prandom_state {
     ((((seed) << (a)) ^ (seed)) >> (b))         \
 )
 
+static inline void
+bfdev_prandom_init(bfdev_prandom_t *pstate)
+{
+    *pstate = BFDEV_PRANDOM_INIT();
+}
+
 /**
  * bfdev_prandom_seed() - set seed for prandom_state.
  * @state: pointer to state structure to receive the seed.
  * @seed: arbitrary 64-bit value to use as a seed.
  */
 extern void
-bfdev_prandom_seed(bfdev_prandom_state_t *pstate, uint64_t seed);
+bfdev_prandom_seed(bfdev_prandom_t *pstate, uint64_t seed);
 
 /**
  * bfdev_prandom_value() - seeded pseudo-random number generator.
  * @state: pointer to state structure holding seeded state.
  */
 extern uint32_t
-bfdev_prandom_value(bfdev_prandom_state_t *pstate);
+bfdev_prandom_value(bfdev_prandom_t *pstate);
 
 static inline uint64_t
-bfdev_prandom_u64(bfdev_prandom_state_t *pstate)
+bfdev_prandom_u64(bfdev_prandom_t *pstate)
 {
     uint32_t high = bfdev_prandom_value(pstate);
     return ((uint64_t)high << 32) + bfdev_prandom_value(pstate);
 }
 
 static inline unsigned long
-bfdev_prandom_long(bfdev_prandom_state_t *pstate)
+bfdev_prandom_long(bfdev_prandom_t *pstate)
 {
 #if BFDEV_BITS_PER_LONG == 32
     return bfdev_prandom_value(pstate);
