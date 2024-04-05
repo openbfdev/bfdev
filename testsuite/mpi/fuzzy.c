1c43a2723e3238b991014c871ec4b78f8b092fe6
John Sanpe <sanpeqf@gmail.com>
2024-04-05 12:02:14 +0800

    feat mpi: use func test each other in fuzzy testsuite
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- testsuite/mpi/fuzzy.c
+++ testsuite/mpi/fuzzy.c
@@ -4,6 +4,7 @@
  */
 
 #include <stdlib.h>
+#include <time.h>
 #include <bfdev/mpi.h>
 #include <testsuite.h>
 
@@ -11,13 +12,16 @@
 #define TEST_SIZE 512
 
 static inline int
-filling_random(bfdev_mpi_t *var, unsigned int length)
+filling_random(bfdev_mpi_t *var)
 {
     BFDEV_MPI_TYPE *buff;
+    unsigned int length;
     size_t size, count;
     int retval;
 
+    length = ((unsigned int)rand() % TEST_SIZE) | 1;
     size = sizeof(*buff) * length;
+
     buff = bfdev_malloc(NULL, size);
     if (bfdev_unlikely(!buff))
         return -BFDEV_ENOMEM;
@@ -25,67 +29,15 @@ filling_random(bfdev_mpi_t *var, unsigned int length)
     for (count = 0; count < size; ++count)
         ((char *)buff)[count] = rand();
 
-    retval = bfdev_mpi_import(var, buff, length, buff[0] & 1);
+    retval = bfdev_mpi_import(var, buff, length, rand() & 1);
     bfdev_free(NULL, buff);
 
     return retval;
 }
 
 TESTSUITE(
-    "mpi:add", NULL, NULL,
-    "mpi add fuzzy test"
-) {
-    unsigned int count;
-    int retval;
-
-    BFDEV_DEFINE_MPI(va, NULL);
-    BFDEV_DEFINE_MPI(vb, NULL);
-
-    if ((retval = filling_random(&va, TEST_SIZE)) ||
-        (retval = filling_random(&vb, TEST_SIZE)))
-        goto failed;
-
-    for (count = 0; count < TEST_LOOP; ++count) {
-        retval = bfdev_mpi_add(&va, &va, &vb);
-        if (retval)
-            goto failed;
-    }
-
-failed:
-    bfdev_mpi_release(&va);
-    bfdev_mpi_release(&vb);
-    return retval;
-}
-
-TESTSUITE(
-    "mpi:sub", NULL, NULL,
-    "mpi sub fuzzy test"
-) {
-    unsigned int count;
-    int retval;
-
-    BFDEV_DEFINE_MPI(va, NULL);
-    BFDEV_DEFINE_MPI(vb, NULL);
-
-    if ((retval = filling_random(&va, TEST_SIZE)) ||
-        (retval = filling_random(&vb, TEST_SIZE)))
-        goto failed;
-
-    for (count = 0; count < TEST_LOOP; ++count) {
-        retval = bfdev_mpi_sub(&va, &va, &vb);
-        if (retval)
-            goto failed;
-    }
-
-failed:
-    bfdev_mpi_release(&va);
-    bfdev_mpi_release(&vb);
-    return retval;
-}
-
-TESTSUITE(
-    "mpi:mul", NULL, NULL,
-    "mpi mul fuzzy test"
+    "mpi:addsub", NULL, NULL,
+    "mpi addsub fuzzy test"
 ) {
     unsigned int count;
     int retval;
@@ -94,14 +46,26 @@ TESTSUITE(
     BFDEV_DEFINE_MPI(va, NULL);
     BFDEV_DEFINE_MPI(vb, NULL);
 
-    if ((retval = filling_random(&va, TEST_SIZE)) ||
-        (retval = filling_random(&vb, TEST_SIZE)))
-        goto failed;
+    srand(time(NULL));
 
+    retval = -BFDEV_ENOERR;
     for (count = 0; count < TEST_LOOP; ++count) {
-        retval = bfdev_mpi_mul(&result, &va, &vb);
+        if ((retval = filling_random(&va)) ||
+            (retval = filling_random(&vb)))
+            goto failed;
+
+        retval = bfdev_mpi_add(&result, &va, &vb);
         if (retval)
             goto failed;
+
+        retval = bfdev_mpi_sub(&result, &result, &vb);
+        if (retval)
+            goto failed;
+
+        if (bfdev_mpi_cmp(&result, &va)) {
+            retval = -BFDEV_EFAULT;
+            goto failed;
+        }
     }
 
 failed:
@@ -112,8 +76,8 @@ failed:
 }
 
 TESTSUITE(
-    "mpi:div", NULL, NULL,
-    "mpi div fuzzy test"
+    "mpi:muldiv", NULL, NULL,
+    "mpi muldiv fuzzy test"
 ) {
     unsigned int count;
     int retval;
@@ -122,18 +86,31 @@ TESTSUITE(
     BFDEV_DEFINE_MPI(va, NULL);
     BFDEV_DEFINE_MPI(vb, NULL);
 
-    if ((retval = filling_random(&va, TEST_SIZE)) ||
-        (retval = filling_random(&vb, TEST_SIZE)))
-        goto failed;
+    srand(time(NULL));
 
-    retval = bfdev_mpi_seti(&vb, 1);
-    if (retval)
-        goto failed;
-
+    retval = -BFDEV_ENOERR;
     for (count = 0; count < TEST_LOOP; ++count) {
-        retval = bfdev_mpi_div(&result, &result, &va, &vb);
+        if ((retval = filling_random(&va)) ||
+            (retval = filling_random(&vb)))
+            goto failed;
+
+        /* Prevent division by zero */
+        retval = bfdev_mpi_bseti(&vb, 0);
         if (retval)
             goto failed;
+
+        retval = bfdev_mpi_mul(&result, &va, &vb);
+        if (retval)
+            goto failed;
+
+        retval = bfdev_mpi_div(&result, &result, &result, &vb);
+        if (retval)
+            goto failed;
+
+        if (bfdev_mpi_cmp(&result, &va)) {
+            retval = -BFDEV_EFAULT;
+            goto failed;
+        }
     }
 
 failed:
