bd0307e759de476d36f3c30621d5f42ff5c9ce65
John Sanpe <sanpeqf@gmail.com>
2024-08-23 03:45:04 +0800

    feat examples: added radix benchmark find loops
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- examples/radix/benchmark.c
+++ examples/radix/benchmark.c
@@ -12,7 +12,8 @@
 #include <bfdev/radix.h>
 #include "../time.h"
 
-#define TEST_LOOP 1000000
+#define TEST_LOOP 10
+#define TEST_SIZE 1000000
 
 static
 BFDEV_DECLARE_RADIX(root, unsigned int);
@@ -20,30 +21,30 @@ BFDEV_DECLARE_RADIX(root, unsigned int);
 int
 main(int argc, const char *argv[])
 {
-    unsigned int count, *data;
+    unsigned int count, loop, *data;
     int retval;
 
-    bfdev_log_info("Generate %u node:\n", TEST_LOOP);
-    data = malloc(sizeof(*data) * TEST_LOOP);
+    bfdev_log_info("Generate %u node:\n", TEST_SIZE);
+    data = malloc(sizeof(*data) * TEST_SIZE);
     if (!data) {
         bfdev_log_err("Insufficient memory!\n");
         return 1;
     }
 
     srand(time(NULL));
-    for (count = 0; count < TEST_LOOP; ++count)
+    for (count = 0; count < TEST_SIZE; ++count)
         data[count] = rand();
 
     root = BFDEV_RADIX_INIT(&root, NULL);
-    retval = bfdev_radix_charge(&root, 0, TEST_LOOP);
+    retval = bfdev_radix_charge(&root, 0, TEST_SIZE);
     if (retval) {
         bfdev_log_err("Insufficient memory!\n");
         return retval;
     }
 
-    bfdev_log_info("Alloc Nodes:\n");
+    bfdev_log_info("Alloc nodes:\n");
     EXAMPLE_TIME_STATISTICAL(
-        for (count = 0; count < TEST_LOOP; ++count) {
+        for (count = 0; count < TEST_SIZE; ++count) {
             unsigned int *block;
             block = bfdev_radix_alloc(&root, count);
             *block = data[count];
@@ -51,22 +52,24 @@ main(int argc, const char *argv[])
         0;
     );
 
-    bfdev_log_info("Find Nodes:\n");
-    EXAMPLE_TIME_STATISTICAL(
-        for (count = 0; count < TEST_LOOP; ++count) {
-            unsigned int *block;
-            block = bfdev_radix_find(&root, count);
-            if (!block || *block != data[count]) {
-                bfdev_log_info("Data verification failed!\n");
-                return 1;
+    for (loop = 0; loop < TEST_LOOP; ++loop) {
+        bfdev_log_info("Find nodes loop%u...\n", loop);
+        EXAMPLE_TIME_STATISTICAL(
+            for (count = 0; count < TEST_SIZE; ++count) {
+                unsigned int *block;
+                block = bfdev_radix_find(&root, count);
+                if (!block || *block != data[count]) {
+                    bfdev_log_info("Data verification failed!\n");
+                    return 1;
+                }
             }
-        }
-        0;
-    );
+            0;
+        );
+    }
 
-    bfdev_log_info("Free Nodes:\n");
+    bfdev_log_info("Free nodes:\n");
     EXAMPLE_TIME_STATISTICAL(
-        for (count = 0; count < TEST_LOOP; ++count) {
+        for (count = 0; count < TEST_SIZE; ++count) {
             retval = bfdev_radix_free(&root, count);
             if (retval) {
                 bfdev_log_info("Sequence error!\n");
