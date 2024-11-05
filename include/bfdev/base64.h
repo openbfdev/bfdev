fb35f690d3035c6bde83e73bc80783dd45907b64
John Sanpe <sanpeqf@gmail.com>
2024-11-05 13:13:57 +0800

    docs crypto: added base32/64 api comments
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- include/bfdev/base64.h
+++ include/bfdev/base64.h
@@ -13,21 +13,41 @@
 
 BFDEV_BEGIN_DECLS
 
+/**
+ * bfdev_base64_encode_length() - base64 encode buffer length.
+ * @size: length to encode.
+ */
 static inline size_t
 bfdev_base64_encode_length(size_t size)
 {
     return BFDEV_DIV_ROUND_UP(size, 3) * 4;
 }
 
+/**
+ * bfdev_base64_decode_length() - base64 decoder buffer length.
+ * @size: length to decode.
+ */
 static inline size_t
 bfdev_base64_decode_length(size_t size)
 {
     return BFDEV_DIV_ROUND_UP(size, 4) * 3;
 }
 
+/**
+ * bfdev_base64_encode() - base64 encoder.
+ * @buff: encoder output buffer.
+ * @data: data to encode.
+ * @size: length of @data.
+ */
 extern void
 bfdev_base64_encode(void *buff, const void *data, size_t size);
 
+/**
+ * bfdev_base64_decode() - base64 decoder.
+ * @buff: decode output buffer.
+ * @data: data to decode.
+ * @size: length of @data.
+ */
 extern int
 bfdev_base64_decode(void *buff, const void *data, size_t size);
 
