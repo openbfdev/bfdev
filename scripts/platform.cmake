9190dc9980d1465b287862ea51fe7274725fe651
John Sanpe <sanpeqf@gmail.com>
2025-04-01 17:06:42 +0800

    feat platform: optimize cross-compile by using try compile to determine types
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- scripts/platform.cmake
+++ scripts/platform.cmake
@@ -3,29 +3,6 @@
 # Copyright(c) 2025 John Sanpe <sanpeqf@gmail.com>
 #
 
-if(HOST_C_COMPILER)
-    set(PRINT_TYPE_LINK "-static")
-endif()
-
-macro(print_type output name)
-    message(STATUS "Print type: " ${name})
-    try_run(
-        RUN_RESULT
-        COMPILE_RESULT
-        ${CMAKE_CURRENT_BINARY_DIR}
-        ${CMAKE_CURRENT_LIST_DIR}/get-type.c
-        COMPILE_DEFINITIONS "-Werror -DTYPE=${name}"
-        LINK_OPTIONS "${PRINT_TYPE_LINK}"
-        RUN_OUTPUT_VARIABLE ${output}
-        COMPILE_OUTPUT_VARIABLE COMPILE_OUTPUT
-    )
-    if ((NOT COMPILE_RESULT) OR (NOT RUN_RESULT EQUAL 0))
-        message("Compile output: ${COMPILE_OUTPUT}")
-        message("Run result: ${RUN_RESULT}")
-        message(FATAL_ERROR "Failed to retrieve the original type ${name}")
-    endif()
-endmacro()
-
 if(CMAKE_SYSTEM_NAME MATCHES "Linux|Android|Darwin|GNU")
     set(BFDEV_PORT_TYPE "posix")
 endif()
@@ -42,20 +19,7 @@ if(CMAKE_SYSTEM_NAME MATCHES "Generic")
     set(BFDEV_PORT_TYPE "generic")
 endif()
 
-if(BFDEV_PORT_TYPE STREQUAL "posix")
-    print_type(BFDEV_TYPE_ADDR "size_t")
-    print_type(BFDEV_TYPE_W64 "int64_t")
-    print_type(BFDEV_TYPE_MAX "intmax_t")
-endif()
-
-if(NOT BFDEV_TYPE_ADDR)
-    set(BFDEV_TYPE_ADDR "long")
-endif()
-
-if(NOT BFDEV_TYPE_W64)
-    set(BFDEV_TYPE_W64 "long long")
-endif()
-
-if(NOT BFDEV_TYPE_MAX)
-    set(BFDEV_TYPE_MAX "long long")
-endif()
+include(${CMAKE_CURRENT_LIST_DIR}/get-type.cmake)
+get_type(BFDEV_TYPE_ADDR "size_t" "long")
+get_type(BFDEV_TYPE_W64 "int64_t" "long long")
+get_type(BFDEV_TYPE_MAX "intmax_t" "long long")
