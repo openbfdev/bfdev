2213df650abd652e79ba4f89ed0cd0579645b5c3
John Sanpe <sanpeqf@gmail.com>
2025-05-02 02:26:17 +0800

    feat array: added append buff api
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- template/array.c
+++ template/array.c
@@ -40,6 +40,21 @@ bfdev_array_append_array(bfdev_array_t *array, const bfdev_array_t *append)
 }
 
 export int
+bfdev_array_append_buff(bfdev_array_t *array, bfdev_buff_t *append)
+{
+    int retval;
+
+    if (array->cells != BFDEV_BYTES_PER_U8)
+        return -BFDEV_EPROTO;
+
+    retval = bfdev_array_append(array, append->data, append->len);
+    if (bfdev_unlikely(retval))
+        return retval;
+
+    return -BFDEV_ENOERR;
+}
+
+export int
 bfdev_array_append_cstr(bfdev_array_t *array, const char *append)
 {
     int retval;
