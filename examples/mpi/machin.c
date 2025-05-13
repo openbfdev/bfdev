e57d7ee4443a676fd138f81c9c39e3f298abd525
John Sanpe <sanpeqf@gmail.com>
2025-05-13 11:40:41 +0800

    refactor mpi: refactoring examples using class raii
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- examples/mpi/machin.c
+++ examples/mpi/machin.c
@@ -20,14 +20,16 @@
 int
 main(int argc, const char *argv[])
 {
-    bfdev_mpi_t vw, vs, vv, vq;
     unsigned int k;
     int retval;
 
-    bfdev_mpi_init(&vw, NULL);
-    bfdev_mpi_init(&vs, NULL);
-    bfdev_mpi_init(&vv, NULL);
-    bfdev_mpi_init(&vq, NULL);
+    BFDEV_CLASS(bfdev_mpi, vw)(NULL);
+    BFDEV_CLASS(bfdev_mpi, vs)(NULL);
+    BFDEV_CLASS(bfdev_mpi, vv)(NULL);
+    BFDEV_CLASS(bfdev_mpi, vq)(NULL);
+
+    if (!vw || !vs || !vv || !vq)
+        return 1;
 
     /**
      * Machin-like formula:
@@ -38,30 +40,30 @@ main(int argc, const char *argv[])
      *  arctan(x) = x - (x^3)/3 + (x^5)/5 - (x^7)/7 + ...
      */
 
-    if ((retval = bfdev_mpi_seti(&vw, 16 * 5)) ||
-        (retval = bfdev_mpi_seti(&vv, 4 * 239)) ||
-        (retval = bfdev_mpi_seti(&vq, 10000)))
+    if ((retval = bfdev_mpi_seti(vw, 16 * 5)) ||
+        (retval = bfdev_mpi_seti(vv, 4 * 239)) ||
+        (retval = bfdev_mpi_seti(vq, 10000)))
         return retval;
 
     for (k = 0; k < TEST_SIZE; ++k) {
-        if ((retval = bfdev_mpi_mul(&vw, &vw, &vq)) ||
-            (retval = bfdev_mpi_mul(&vv, &vv, &vq)))
+        if ((retval = bfdev_mpi_mul(vw, vw, vq)) ||
+            (retval = bfdev_mpi_mul(vv, vv, vq)))
             return retval;
     }
 
     bfdev_log_info("Convergence Machin %d:\n", TEST_LEN);
     EXAMPLE_TIME_STATISTICAL(
         for (k = 1; k <= TEST_LOOP; ++k) {
-            if ((retval = bfdev_mpi_divi(&vw, &vw, &vw, 5 * 5)) ||
-                (retval = bfdev_mpi_divi(&vv, &vv, &vv, 239 * 239)) ||
-                (retval = bfdev_mpi_sub(&vq, &vw, &vv)) ||
-                (retval = bfdev_mpi_divi(&vq, &vq, &vq, 2 * k - 1)))
+            if ((retval = bfdev_mpi_divi(vw, vw, vw, 5 * 5)) ||
+                (retval = bfdev_mpi_divi(vv, vv, vv, 239 * 239)) ||
+                (retval = bfdev_mpi_sub(vq, vw, vv)) ||
+                (retval = bfdev_mpi_divi(vq, vq, vq, 2 * k - 1)))
                 return retval;
 
             if (k & 1)
-                retval = bfdev_mpi_add(&vs, &vs, &vq);
+                retval = bfdev_mpi_add(vs, vs, vq);
             else
-                retval = bfdev_mpi_sub(&vs, &vs, &vq);
+                retval = bfdev_mpi_sub(vs, vs, vq);
 
             if (retval)
                 return retval;
@@ -72,7 +74,7 @@ main(int argc, const char *argv[])
 #if PRINT_RESULT
     char *result;
 
-    result = print_num(&vs, 10);
+    result = print_num(vs, 10);
     if (!result)
         return 1;
 
@@ -80,11 +82,6 @@ main(int argc, const char *argv[])
     puts(result + 1);
     free(result);
 #endif
-
-    bfdev_mpi_release(&vw);
-    bfdev_mpi_release(&vs);
-    bfdev_mpi_release(&vv);
-    bfdev_mpi_release(&vq);
 
     return 0;
 }
