56b16012ba56efcf897651472066a7a8a7b40c28
Zhenlin Wang <sanpeqf@gmail.com>
2025-08-29 15:13:37 +0800

    feat modules: added log hostname, localtime and pid modules
    Signed-off-by: Zhenlin Wang <sanpeqf@gmail.com>

---
+++ modules/build.cmake
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# Copyright(c) 2025 Zhenlin Wang <sanpeqf@gmail.com>
+#
+
+set(BFDEV_SOURCE
+    ${BFDEV_SOURCE}
+    ${CMAKE_CURRENT_LIST_DIR}/log-hostname.c
+    ${CMAKE_CURRENT_LIST_DIR}/log-localtime.c
+    ${CMAKE_CURRENT_LIST_DIR}/log-pid.c
+)
