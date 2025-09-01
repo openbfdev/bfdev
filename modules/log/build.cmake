5543ca4d8ce53816f96fcf200f5ecd647cdd7ce1
Zhenlin Wang <sanpeqf@gmail.com>
2025-09-01 22:35:48 +0800

    feat log: added syslog module
    Signed-off-by: Zhenlin Wang <sanpeqf@gmail.com>

--- modules/log/build.cmake
+++ modules/log/build.cmake
@@ -8,14 +8,17 @@ set(BFDEV_SOURCE
     ${CMAKE_CURRENT_LIST_DIR}/chain-hostname.c
     ${CMAKE_CURRENT_LIST_DIR}/chain-localtime.c
     ${CMAKE_CURRENT_LIST_DIR}/chain-pid.c
+    ${CMAKE_CURRENT_LIST_DIR}/write-syslog.c
 )
 
 check_header_and_exit("time.h")
 check_header_and_exit("unistd.h")
+check_header_and_exit("syslog.h")
 check_header_and_exit("sys/utsname.h")
 
 check_symbol_and_exit("time.h" "time")
 check_symbol_and_exit("time.h" "localtime")
 check_symbol_and_exit("time.h" "strftime")
 check_symbol_and_exit("unistd.h" "getpid")
+check_symbol_and_exit("syslog.h" "syslog")
 check_symbol_and_exit("sys/utsname.h" "uname")
